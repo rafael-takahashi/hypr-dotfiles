353880d11506b051fdfc24ebace6175d8d7767b9
Rafael Takahashi <rafael.takahashi20@outlook.com>
2025-08-14 09:03:33 -0300

    fix: use master branch in treesitter setup

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = "main",
+  branch = "master",
   lazy = false,
   build = ":TSUpdate",
   opts = {
     highlight = { enable = true },
     indent = { enable = true },
     ensure_installed = {
-      "c", "lua", "vim", "vimdoc", "javascript", "typescript", "python"
+      "c", "lua", "javascript", "typescript", "cpp", "python"
     },
   },
   config = function(_, opts)
