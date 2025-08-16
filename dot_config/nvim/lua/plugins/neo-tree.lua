c4298decca87d8ca4cde9680737dedd9f7d9a5a2
Rafael Takahashi <rafael.takahashi20@outlook.com>
2025-08-16 10:17:43 -0300

    feat: add toggle neo tree and set number

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -9,7 +9,7 @@ return {
     },
     lazy = false,
     config = function()
-      vim.keymap.set("n", "<leader>t", ":Neotree filesystem reveal left<CR>", {})
+      vim.keymap.set("n", "<leader>t", ":Neotree reveal toggle left<CR>", {})
     end,
   },
 }
